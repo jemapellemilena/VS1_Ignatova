60dbeaf85286be48c54d92585da06fb566d775fb
Лавелин Эдуард <ed.lav.00@mail.ru>
2023-12-01 16:52:04 +0500

    Добавление библиотеки Font-Awesome (иконки как шрифт)

---
+++ VS1_Ignatova/Models/AppCtx.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VS1_Ignatova.Models
+{
+    public class AppCtx : IdentityDbContext<User>
+    {
+        public AppCtx(DbContextOptions<AppCtx> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        /*public DbSet<FormOfStudy> FormsOfStudy { get; set; }
+        public DbSet<Specialty> Specialties { get; set; }
+
+        public DbSet<Group> Groups { get; set; }*/
+    }
+}
---
+++ VS1_Ignatova/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace VS1_Ignatova.Models
+{
+    public class User : IdentityUser
+    {
+        //дополнительные поля для каждого пользователя
+        //для преподавателя могут понадобиться данные о ФИО
+
+        [Required(ErrorMessage = "Введите фамилию")]   // сообщение об ошибке при валидации на стороне клиента
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }   //отображение Фамилия вместо LastName
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+        public string Patronymic { get; set; }
+
+
+        //навигационные свойства
+       /* [Required]
+        public ICollection<FormOfStudy> FormsOfStudy { get; set; }*/
+    }
+}
