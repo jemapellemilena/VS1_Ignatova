f25956091365fd84f452c0b1840ac81eed80d80e
Игнатова Милена <milenaign23@gmail.com>
2023-12-13 12:00:34 +0500

    окна открываются

--- VS1_Ignatova/Controllers/AccountController.cs
+++ VS1_Ignatova/Controllers/AccountController.cs
@@ -112,22 +112,20 @@ namespace VS1_Ignatova.Controllers
 
                 var result =
                     await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
-                if (result.Succeeded)
-                {
-                    // назначение роли "Зарегистрированный пользователь"
-                    await _userManager.AddToRoleAsync(user, "registeredUser");
+            if (result.Succeeded)
+            {
 
-                   return RedirectToAction("Index", "Home");
-                }
-                    // проверяем, принадлежит ли URL приложению
-                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
-                    {
-                        return Redirect(model.ReturnUrl);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
+                return RedirectToAction("Index", "Home");
+            }
+            // проверяем, принадлежит ли URL приложению
+            if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+            {
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
             /*else
                 {
                     ModelState.AddModelError("", "Неправильный логин и (или) пароль");
